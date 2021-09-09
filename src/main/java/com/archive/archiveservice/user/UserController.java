d29e1460b03ddcd669192abc00c4b2ede3a0fc94
ayo olaniyan <ayobami.olaniyan@ingka.com>
2021-09-09 17:19:32 +0200

    inital commit

---
+++ src/main/java/com/archive/archiveservice/user/UserController.java
@@ -0,0 +1,46 @@
+package com.archive.archiveservice.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserDaoService userDaoService;
+
+    @GetMapping(path = "/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping(path = "/user/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        User user = userDaoService.findOne(id);
+        if(user ==null)
+            throw new UserNotFoundException("id-"+ id);
+        return user;
+    }
+
+    @PostMapping(path = "/users")
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+        User savedUser = userDaoService.save(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping(path = "/user/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = userDaoService.deleteById(id);
+        if(user ==null)
+            throw new UserNotFoundException("id-"+ id);
+    }
+}
